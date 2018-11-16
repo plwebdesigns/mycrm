74a2ddc49c085e9dbe38288aec02bba2d185b547
plongo <paullongo@outlook.com>
2018-11-16 13:58:53 -0500

    added createEmployee template view

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def createEmployee(request):
+    return render(request, 'employees/createEmployee.html')
--- mycrm/urls.py
+++ mycrm/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', include('indexapp.urls')),
+    path('createEmployee/', include('employees.urls')),
 ]
