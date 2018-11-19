9132ee4c13f9c2ada4c33cae39facaca96f5a90c
plongo <paullongo@outlook.com>
2018-11-19 18:07:08 -0500

    added clientList page-partial

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def clientList(request):
+	return render(request, 'clients/clientList.html')
--- mycrm/urls.py
+++ mycrm/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include('indexapp.urls')),
+    path('', include('indexapp.urls')),
     path('createEmployee/', include('employees.urls')),
+    path('clientList/', include('clients.urls')),
 ]
