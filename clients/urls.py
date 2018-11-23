ef9715c3bb5aa296ab6195af273b6b44878ff75e
plongo <paullongo@outlook.com>
2018-11-23 13:08:43 -0500

    attempting deploy to heroku

--- clients/urls.py
+++ clients/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'clients'
 urlpatterns = [
 	path('', views.clientList, name='clientList'),
 	path('<int:id_client>/', views.detailView, name='detailView'),
